10d1f4124c3366a66941286bfec2adf9c4ef907a
David Browning <davidbr101@hotmail.com>
2019-04-08 15:36:23 -0700

    Added std::ring class. Added public functions for ring.

---
+++ ring/ring/include/ring.h
@@ -0,0 +1,100 @@
+#pragma once
+#include <vector>
+
+namespace std
+{
+	/*
+	 A ring holds a fixed number of elements and uses a pointer to indicate the
+	 current item.
+	 The pointer can be advanced to access the next element in a clockwise
+	 order. A ring has does not have a first or last element. Continually 
+	 advancing the ring pointer will bring the pointer back to where is 
+	 originally was.
+	 The pointer can be retreated to access the previous element in a 
+	 counter-clockwise order.
+	 Accessing the current element and advancing and retreating the pointer are
+	 constant operations.
+	 */
+	template<class T, class Alloc = allocator<T>>
+	class ring
+	{
+		using value_type = T;
+		using allocator_type = Alloc;
+
+		public:
+		/*
+		 Construct an empty ring with "capacity" slots.
+		 */
+		ring(size_t capacity);
+
+		/*
+		 Construct a ring using an iterator.
+		 */
+		template<class InputIterator>
+		ring(InputIterator first,
+			 InputIterator last,
+			 const allocator_type& alloc = allocator_type());
+
+		/*
+		 Construct a ring using an initializer list.
+		 */
+		ring(initializer_list<value_type> il,
+			 const allocator_type& alloc = allocator_type());
+
+		/*
+		 Copy Constructor
+		 */
+		ring(const ring& x) = default;
+
+		/*
+		 Move Constructor
+		 */
+		ring(ring&& x) = default;
+
+		/*
+		 Destructor
+		 */
+		~ring() = default;
+
+		/*
+		 Advances the ring pointer to the next item in the ring.
+		 Complexity: Constant
+		 */
+		void advance();
+
+		/*
+		 Advances the ring pointer to the previous item in the ring.
+		 Complexity: Constant
+		 */
+		void retreat();
+
+		/*
+		 Returns the number of elements in the ring.
+		 Complexity: Constant
+		 */
+		size_t size() const noexcept;
+
+		/*
+		 Returns a reference to the current element pointed to in the ring.
+		 Complexity: Constant
+		 */
+		value_type& current();
+
+		/*
+		 Returns a const-reference to the current element pointed to in the ring.
+		 Complexity: Constant
+		 */
+		const value_type& current() const;
+
+		private:
+		/*
+		 Index of the current item.
+		 */
+		size_t m_current;
+
+		/*
+		 Underlying container.
+		 */
+		std::vector<value_type, allocator_type> m_container;
+	};
+}

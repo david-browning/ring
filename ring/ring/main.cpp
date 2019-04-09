b68a4662eb71b57dc5d20ce283f71816d33add42
David Browning <davidbr101@hotmail.com>
2019-04-09 13:33:46 -0700

    Test driver calls testing apparatus.

--- ring/ring/main.cpp
+++ ring/ring/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "tests/ring_tests.h"
+#include <cassert>
 
 int main(int argc, char** argv)
 {
+	ring::test::ring_tests tests;
+	assert(tests.test() == true);
+
 	return 0;
 }

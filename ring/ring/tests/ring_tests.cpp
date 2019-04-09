03425aaeda815561ddb034ce64b2022d32671cf3
David Browning <davidbr101@hotmail.com>
2019-04-09 13:36:29 -0700

    Added stubs for testing apparatus.

---
+++ ring/ring/tests/ring_tests.cpp
@@ -0,0 +1,41 @@
+#include "ring_tests.h"
+
+bool ring::test::ring_tests::test() const
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_capacity_constructor() const
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_iterator_constructor() const
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_il_constructor() const
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_copy_constructor() const
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_move_constructor()
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_advance()
+{
+	return false;
+}
+
+bool ring::test::ring_tests::test_retreat()
+{
+	return false;
+}
